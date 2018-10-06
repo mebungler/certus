27ec6d73311e6da5d03a075492f2b40628f90041
HarrekeHippoVic <pro.hippovic.85@gmail.com>
2018-10-06 18:03:58 +0500

    Passport changes

--- Server/data/models/passport.go
+++ Server/data/models/passport.go
@@ -1,13 +1,13 @@
 package models
 
 type Passport struct {
-	ID string `gorm:"primary_key" json:"id,omitempty" "AUTO_INCREMENT"`
-	CodeOfPassport int
+	ID string `gorm:"primary_key" json:"id,omitempty"`
+	CodeOfPassport int   `json:"codeOfPassport"`
 	Customer Customer	`json:"customer"`
 	CustomerID string
-	Model Model     `json:"model"`
+	Model Model        `json:"model"`
 	ModelID string
-	Color string	`json:"color"`
-	Size int		`json:"size"`
-	Quantity int   `json:"quantity"`
+	Color string	   `json:"color"`
+	Size int		   `json:"size"`
+	Quantity int       `json:"quantity"`
 }
